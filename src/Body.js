0b7247d1241da49366db17e6ce9fb5b7c582b602
Vladislav Gushchin <vladislav.g@cronix.ms>
2018-07-30 16:55:34 +0300

    adding

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
+// import ReactDom from 'react-dom'
+
 class Body extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            n_task: '',
             tasks: ['task1', 'task2']
         }
+    }
+
+    setTaskValue = (e) => {
+        this.setState({n_task: e.target.value})
+    }
+
+    addTask = () => {
+        this.setState({
+            tasks: [...this.state.tasks, this.state.n_task]
+        })
     }
 
     render() {
@@ -17,8 +30,8 @@ class Body extends React.Component {
         return (
             <div className="body-inner">
                 <div className="create-task">
-                    <input type="text"/>
-                    <button type="button" className="btn">Add</button>
+                    <input type="text" defaultValue="" placeholder="any task" ref="task" onChange={this.setTaskValue}/>
+                    <button type="button" className="btn" onClick={this.addTask}>Add</button>
                 </div>
                 <div className="table">
                     <strong className="strong">List</strong>
