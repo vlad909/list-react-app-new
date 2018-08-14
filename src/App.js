b8f74c3460f1a0d62c4c1b5d7604af5aff4b96b2
Vladislav Gushchin <vladislav.g@cronix.ms>
2018-08-14 19:02:36 +0300

    start redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Body from './Body.jsx'
+
 // import ee from 'event-emitter'
 
 class App extends Component {
---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+export const someData = text => ({
+    type: 'someData',
+    payload: text
+})
